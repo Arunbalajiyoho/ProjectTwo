3ce0ec870ac94e578c34b165ebe5bdda62c3d328
Arunbalajiyoho <140370898+Arunbalajiyoho@users.noreply.github.com>
2023-08-01 17:44:17 +0530

    arunbalaji commit

---
+++ src/constants/index.js
@@ -0,0 +1,82 @@
+const HomeCategory = [
+    {
+        id: 1,
+        name : "Atelier Dietmar Schönherr | Branding",
+        image : "https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/322482143396599.Y3JvcCwxMDA3LDc4OCwxOTcsMA.jpg",
+        userName : "Hochburg Design",
+        likes :139 ,
+        views :"1.4k" ,
+        categoryName : "Projects"
+        
+    },
+    {
+        id: 2,
+        name : "Funny",
+        image : "https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/991621137076253.Y3JvcCwzMTA3LDI0MzEsMTg0LDA.jpg",
+        userName : "Maryia Nesterovich",
+        likes :688 ,
+        views : "4.3K" ,
+        categoryName : "Projects"
+        
+    },
+    {
+        id: 3,
+        name : "The Order of Nature",
+        image : "https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/670c6f144935117.Y3JvcCwxMTkyLDkzMyw4NSww.jpg",
+        userName : "Leah Kennedy",
+        likes :167,
+        views : 843,
+        categoryName : "Projects"
+        
+    },
+    {
+        id: 4,
+        name : "Cheers to the golden age!",
+        image : "https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/d4e2fe175284359.Y3JvcCwyOTQ1LDIzMDQsNTc2LDA.jpg",
+        userName : "Tiger Pan ",
+        likes :168 ,
+        views : "1.6k",
+        categoryName : "Projects"
+        
+    },
+    {
+        id: 5,
+        name : "BOXER",
+        image : "https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/f1d044176194353.Y3JvcCw0NTAwLDM1MTksMCww.jpg",
+        userName : "Vladislav Ociacia",
+        likes :72 ,
+        views : 711,
+        categoryName : "Projects"
+        
+    },
+    {
+        id: 6,
+        name : "Portrait study",
+        image : "https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/70b207176457851.Y3JvcCwxNTY2LDEyMjUsMjEzLDUxOQ.png",
+        userName : "Jéssica Brasil",
+        likes : 130,
+        views : 685,
+        categoryName : "Projects"
+        
+    },
+    {
+        id: 7,
+        name : "The Richmond Forum Opener - Director's Cut",
+        image : "https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/5e04c7171881597.Y3JvcCwxMzgwLDEwODAsMjU2LDA.jpg",
+        userName : "Multiple owners",
+        likes : 612,
+        views :"3.9K" ,
+        categoryName : "Projects"
+        
+    },
+    {
+        id: 8,
+        name : "Beautiful Andes",
+        image : "https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/d42737172884709.648b7338bb468.jpg",
+        userName : "Jakub Sanek",
+        likes :152 ,
+        views :"1.2K " ,
+        categoryName : "Projects"
+        
+    }
+]
