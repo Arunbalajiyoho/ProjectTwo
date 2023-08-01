beb6b68ad736ffdd25feebf1aded5be64bc5bf53
Rajeshyoho <140370925+Rajeshyoho@users.noreply.github.com>
2023-08-01 17:52:12 +0530

    Rajesh commit

---
+++ src/constants/index.js
@@ -0,0 +1,143 @@
+const ForYou=[
+{
+
+ id:1,
+ images:[
+    {
+        url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/7c1a60175225273.Y3JvcCwxMDA3LDc4OCwxOTcsMA.png"
+    },
+
+
+    {
+         url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/b130a8175824203.Y3JvcCwzOTAxLDMwNTEsNDgyLDYyNw.jpg"
+    },{
+        url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/b4fc7f142357547.Y3JvcCwyMTMxLDE2NjcsMjY5LDA.png"
+    }
+ ],
+
+ name:"Graphic Design"
+
+
+
+
+},
+
+{
+
+    id:2,
+    images:[
+       {
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/3c17ae172718485.Y3JvcCwxNDQwLDExMjYsMCw2MA.jpeg"
+       },
+   
+   
+       {
+            url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/ac6cab174542423.64a4509ac7740.jpg"
+       },{
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/b3f7b7172611369.Y3JvcCwxOTk5LDE1NjQsMCwxOQ.jpg"
+       }
+    ],
+   
+    name:"Fine Arts"
+   
+   
+   
+   
+   },
+
+
+   {
+
+    id:3,
+    images:[
+       {
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/d499b1175672567.Y3JvcCwzMjAwLDI1MDIsMCwxMDY5.jpg"
+       },
+   
+   
+       {
+            url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/97e8be175911483.Y3JvcCwzOTk5LDMxMjgsMCwzMzY.jpg"
+       },{
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/6257d2133035883.Y3JvcCwzNzQ0LDI5MjgsMCw4NzE.jpg"
+       }
+    ],
+   
+    name:"Photography"
+   
+   
+   
+   
+   },
+
+
+   {
+
+    id:4,
+    images:[
+       {
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/95cbf4170359545.Y3JvcCw5NTIsNzQ0LDAsMTg2.jpg"
+       },
+   
+   
+       {
+            url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/e4b061114055113.Y3JvcCwxMTg3LDkyOSwxMDUsMA.jpg"
+       },{
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/b64956169431583.Y3JvcCw1MTQ0LDQwMjQsNTIyLDA.jpg"
+       }
+    ],
+   
+    name:"Interior Design"
+   
+   
+   
+   
+   },
+
+   {
+
+    id:5,
+    images:[
+       {
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/0c796e173465411.Y3JvcCw4MDgsNjMyLDAsMA.png"
+       },
+   
+   
+       {
+            url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/d49909172948999.649251c3b00c4.jpg"
+       },{
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/f97a62171487549.Y3JvcCwxNjE2LDEyNjQsMCww.jpg"
+       }
+    ],
+   
+    name:"Icon Design"
+   
+   
+   
+   
+   },
+
+   {
+
+    id:6,
+    images:[
+       {
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/b90de3176366065.Y3JvcCwzMDAwLDIzNDYsMCw2OTA.jpg"
+       },
+   
+   
+       {
+            url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/1af6dc175566953.Y3JvcCwxMTMyLDg4NSw2Nyw4MQ.jpg"
+       },{
+           url:"https://mir-s3-cdn-cf.behance.net/projects/max_808/9d28e8175275079.Y3JvcCwyMzIzLDE4MTcsMCwzODU.jpg"
+       }
+    ],
+   
+    name:"Street Art"
+   
+   
+   
+   
+   },
+
+
+]
