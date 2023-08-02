06e04926505af881c31ca3b9ca4d791fb988e2cc
SureshYoho <140370780+SureshYoho@users.noreply.github.com>
2023-08-02 10:35:03 +0530

    suresh commit

---
+++ src/constants/index.js
@@ -0,0 +1,210 @@
+const Card =[
+    { id:1,
+      Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/4a478a173864607.Y3JvcCwxNzU2LDEzNzQsMzQ3LDA.png" ,
+      likes:"11k", 
+      views:"20k",
+      price:"18$",
+      name:"Student || Free Download ",
+      Categoriesname:"Fonts"
+    },
+
+
+    { id:2,
+        Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/8a63c8166599103.Y3JvcCwyOTIwLDIyODQsODAyLDEzMzA.jpg" ,
+        likes:"21k", 
+        views:"30k",
+        price:"25$",
+        name:"Fandacy Twings  ",
+        Categoriesname:"Fonts"
+  
+      },
+
+
+
+      { id:3,
+        Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/681826170593705.Y3JvcCwxNzg5LDE0MDAsMTA2LDA.png" ,
+        likes:"32k", 
+        views:"80k",
+        price:"22$",
+        name:"illustrator for advertisement",
+        Categoriesname:"Fonts"
+  
+      },
+
+
+
+      { id:4,
+        Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/382d97172252213.Y3JvcCwxOTIwLDE1MDEsMCw0OTk.jpg" ,
+        likes:"22k", 
+        views:"60k",
+        price:"45$",
+        name:"Ai Monkey Art ",
+        Categoriesname:"Fonts"
+  
+      },
+
+
+      { id:5,
+        Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/bedc9b165509527.Y3JvcCwxNTUyLDEyMTQsMTM0LDA.png" ,
+        likes:"55k", 
+        views:"80k",
+        price:"79$",
+        name:"Type Family",
+        Categoriesname:"Fonts"
+  
+      },
+
+
+
+      { id:6,
+        Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/bb3b1c168039351.Y3JvcCw5ODEsNzY4LDM0LDA.jpeg" ,
+        likes:"66k", 
+        views:"89k",
+        price:"50$",
+        name:"LANDMANLING",
+        Categoriesname:"Fonts"
+  
+      },
+ 
+     
+     
+      { id:7,
+        Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/00e9ba171804287.Y3JvcCwyNTAwLDE5NTUsMCww.png" ,
+        likes:"1k", 
+        views:"28k",
+        price:"17$",
+        name:"Bundles Of Flowers ",
+        Categoriesname:"illustrations"
+      },
+  
+  
+      { id:8,
+          Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/8a63c8166599103.Y3JvcCwyOTIwLDIyODQsODAyLDEzMzA.jpg" ,
+          likes:"31k", 
+          views:"44k",
+          price:"45$",
+          name:"Fandacy Twings  ",
+          Categoriesname:"illustrations"
+    
+        },
+  
+  
+  
+        { id:9,
+          Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/681826170593705.Y3JvcCwxNzg5LDE0MDAsMTA2LDA.png" ,
+          likes:"32k", 
+          views:"80k",
+          price:"22$",
+          name:"illustrator for advertisement",
+          Categoriesname:"illustrations"
+    
+        },
+
+
+
+
+        { id:10,
+            Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/382d97172252213.Y3JvcCwxOTIwLDE1MDEsMCw0OTk.jpg" ,
+            likes:"22k", 
+            views:"60k",
+            price:"45$",
+            name:"Ai Monkey Art ",
+            Categoriesname:"Photos"
+      
+          },
+    
+    
+          { id:11,
+            Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/bedc9b165509527.Y3JvcCwxNTUyLDEyMTQsMTM0LDA.png" ,
+            likes:"55k", 
+            views:"80k",
+            price:"79$",
+            name:"Type Family",
+            Categoriesname:"Photos"
+      
+          },
+    
+    
+    
+          { id:12,
+            Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/bb3b1c168039351.Y3JvcCw5ODEsNzY4LDM0LDA.jpeg" ,
+            likes:"66k", 
+            views:"89k",
+            price:"50$",
+            name:"LANDMANLING",
+            Categoriesname:"Photos"
+      
+          },
+
+
+
+          { id:13,
+            Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/00e9ba171804287.Y3JvcCwyNTAwLDE5NTUsMCww.png" ,
+            likes:"1k", 
+            views:"28k",
+            price:"17$",
+            name:"Bundles Of Flowers ",
+            Categoriesname:"Templates"
+          },
+      
+      
+          { id:14,
+              Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/8a63c8166599103.Y3JvcCwyOTIwLDIyODQsODAyLDEzMzA.jpg" ,
+              likes:"31k", 
+              views:"44k",
+              price:"45$",
+              name:"Fandacy Twings  ",
+              Categoriesname:"Templates"
+        
+            },
+      
+      
+      
+            { id:15,
+              Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/681826170593705.Y3JvcCwxNzg5LDE0MDAsMTA2LDA.png" ,
+              likes:"32k", 
+              views:"80k",
+              price:"22$",
+              name:"illustrator for advertisement",
+              Categoriesname:"Templates"
+        
+            },
+
+
+
+            { id:16,
+                Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/382d97172252213.Y3JvcCwxOTIwLDE1MDEsMCw0OTk.jpg" ,
+                likes:"22k", 
+                views:"60k",
+                price:"45$",
+                name:"Ai Monkey Art ",
+                Categoriesname:"Vectors"
+          
+              },
+        
+        
+              { id:17,
+                Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/bedc9b165509527.Y3JvcCwxNTUyLDEyMTQsMTM0LDA.png" ,
+                likes:"55k", 
+                views:"80k",
+                price:"79$",
+                name:"Type Family",
+                Categoriesname:"Vectors"
+          
+              },
+        
+        
+        
+              { id:18,
+                Imageuri:"https://mir-s3-cdn-cf.behance.net/projects/max_808_webp/bb3b1c168039351.Y3JvcCw5ODEsNzY4LDM0LDA.jpeg" ,
+                likes:"66k", 
+                views:"89k",
+                price:"50$",
+                name:"LANDMANLING",
+                Categoriesname:"Vectors"
+          
+              }
+    
+
+
+]
